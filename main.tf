af59cb57eb5fa9ae7cdfaacb5cca8c2798af7253
Lakshmi Prasann <lakshmiprasannak1611@gmail.com>
2026-06-27 08:00:12 +0000

    Fix terraform AMI data source

--- main.tf
+++ main.tf
@@ -97,7 +97,21 @@ resource "aws_security_group" "web_sg" {
 # -----------------------------
 # Latest Amazon Linux 2023 AMI
 # -----------------------------
-ami = data.aws_ami.amazon_linux.id
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
 # -----------------------------
 # EC2 Instance
 # -----------------------------
